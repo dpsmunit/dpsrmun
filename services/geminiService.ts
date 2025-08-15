05df86313999ccc6881ec4d24b8d66f3047e0618
MrSachchidanand13 <cloudcomputerdps@gmail.com>
2025-08-15 15:07:45 +0530

    removed the env
    it is not working

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,11 +1,11 @@
 import { GoogleGenAI, Chat } from "@google/genai";
 
-const API_KEYS = [];
-let i = 1;
-while (process.env[`GEM${i}`]) {
-  API_KEYS.push(process.env[`GEM${i}`].trim());
-  i++;
-}
+const API_KEYS = [
+  "AIzaSyBurETbQFkvTQtnCypMmwmOH4h4QuFIbFU",
+  "AIzaSyA8stqKIOC3H1rb5sHuS0_dmxNYof3utT8",
+  "AIzaSyCpFlhdmvMa5Rp9AQvRBBAnZB0Iv5ageR0"
+  // Add more API keys as needed
+];
 
 interface ApiKeyStatus {
   index: number;
