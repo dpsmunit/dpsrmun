9f910467a5336ba139b3ad17d2baa6ef7954f562
Kai <kail266rb@gmail.com>
2025-08-21 13:25:18 +0530

    Update GalleryPage.tsx

--- pages/GalleryPage.tsx
+++ pages/GalleryPage.tsx
@@ -74,7 +74,7 @@ const GALLERY_ITEMS = [
     { type: 'image', src: './images/photos/img12.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
     { type: 'image', src: './images/photos/img15.avif', thumbnailSrc: '', caption: 'BCCI', tags: ['Image'] },
     { type: 'image', src: './images/photos/img16.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
-    { type: 'image', src: './images/photos/img17.avif', thumbnailSrc: '', caption: 'BCCI', tags: ['Image'] },
+    { type: 'image', src: './images/photos/img17.avif', thumbnailSrc: '', caption: 'FIFA', tags: ['Image'] },
     { type: 'image', src: './images/photos/img18.avif', thumbnailSrc: '', caption: 'UNSCW', tags: ['Image'] },   
     { type: 'image', src: './images/photos/img19.avif', thumbnailSrc: '', caption: 'UNSC', tags: ['Image'] },
     { type: 'image', src: './images/photos/img20.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
