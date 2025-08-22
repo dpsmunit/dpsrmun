b2c4324e130637282ded37c3a414224940f9f57b
Kai <kail266rb@gmail.com>
2025-08-22 09:07:38 +0530

    Update GalleryPage.tsx

--- pages/GalleryPage.tsx
+++ pages/GalleryPage.tsx
@@ -71,14 +71,14 @@ const GALLERY_ITEMS = [
    { type: 'image', src: './images/photos/img6.avif', thumbnailSrc: '', caption: 'Opening Ceremony', tags: ['Image'] },
   { type: 'image', src: './images/photos/img9.avif', thumbnailSrc: '', caption: 'Opening Ceremony', tags: ['Image'] },
    { type: 'image', src: './images/photos/img10.avif', thumbnailSrc: '', caption: 'Opening Ceremony', tags: ['Image'] },
+    { type: 'image', src: './images/photos/img16.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
+    { type: 'image', src: './images/photos/img20.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
+    { type: 'image', src: './images/photos/img21.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
     { type: 'image', src: './images/photos/img12.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
     { type: 'image', src: './images/photos/img15.avif', thumbnailSrc: '', caption: 'BCCI', tags: ['Image'] },
-    { type: 'image', src: './images/photos/img16.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
     { type: 'image', src: './images/photos/img17.avif', thumbnailSrc: '', caption: 'FIFA', tags: ['Image'] },
     { type: 'image', src: './images/photos/img18.avif', thumbnailSrc: '', caption: 'UNSCW', tags: ['Image'] },   
     { type: 'image', src: './images/photos/img19.avif', thumbnailSrc: '', caption: 'UNSC', tags: ['Image'] },
-    { type: 'image', src: './images/photos/img20.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
-    { type: 'image', src: './images/photos/img21.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] },
     { type: 'image', src: './images/photos/img22.avif', thumbnailSrc: '', caption: 'Closing Ceremony', tags: ['Image'] },
     { type: 'image', src: './images/photos/img23.avif', thumbnailSrc: '', caption: 'Loksabha', tags: ['Image'] }
 ];
