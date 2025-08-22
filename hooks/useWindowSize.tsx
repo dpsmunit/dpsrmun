663ba422298cc5a81ea7b3a776ca5e41b14312b1
Kai <kail266rb@gmail.com>
2025-08-22 10:39:08 +0530

    Update useWindowSize.tsx

--- hooks/useWindowSize.tsx
+++ hooks/useWindowSize.tsx
@@ -1,33 +1,36 @@
+
 import { useState, useEffect } from 'react';
 
-interface WindowSize { 
-      width: number; 
-      height: number;
-} 
+interface WindowSize {
+  width: number;
+  height: number;
+}
 
-const useWindowSize = (): WindowSize => { 
-  const [windowSize, setWindowSize] = useState<WindowSize>({ 
+const useWindowSize = (): WindowSize => {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
     width: typeof window !== 'undefined' ? window.innerWidth : 0,
     height: typeof window !== 'undefined' ? window.innerHeight : 0,
   });
-      
-  useEffect(() => { 
+
+  useEffect(() => {
     if (typeof window === 'undefined') {
-      return;
-    } 
-        
+        return;
+    }
+    
     const handleResize = () => {
       setWindowSize({
-        width: window.innerWidth, 
+        width: window.innerWidth,
         height: window.innerHeight,
-      }); 
-    }; 
-    
-    window.addEventListener('resize', handleResize); handleResize(); // Initial call 
-    return () => window.removeEventListener('resize', handleResize); 
-  }, []); 
-  
-  return windowSize; 
-}; 
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial call
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+};
 
 export default useWindowSize;
