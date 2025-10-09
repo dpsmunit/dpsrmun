563ffb553fe0dfef13f6a8210fdb7de263bb18e6
MrSachchidanand13 <cloudcomputerdps@gmail.com>
2025-10-09 18:20:45 +0530

    Update assets.ts

--- assets.ts
+++ assets.ts
@@ -4,20 +4,10 @@ export const assetPaths = {
     
     // Team Avatars
 
- // gyty: 'https://i.ibb.co/5WLRJyCx/aradhya.jpg',
-  //priyaSingh: 'https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg',
- // rohanMehta: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
- // anikaReddy: 'https://images.pexels.com/photos/1239288/pexels-photo-1239288.jpeg',
- // kavyaMenon: 'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg',
-  //arjunDesai: 'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg',
- // saanviGupta: 'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg',
- // ishaanPatel: 'https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg',
-//  myraKumar: 'https://images.pexels.com/photos/2787341/pexels-photo-2787341.jpeg',
- // vikramSingh: 'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg',
 
     // Page specific images
     aboutVision: '/images/photos/imgdel_disc.avif',
    aboutFuture: '/images/photos/imgglbal.avif',
-   sgNote: '/team/aaradhya.avif',
+   sgNote: '/team/aaradhya.jpg',
     directorGeneral:'/team/suvigya.avif'
 };
